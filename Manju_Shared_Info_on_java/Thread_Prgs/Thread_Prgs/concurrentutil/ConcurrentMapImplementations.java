55dab1fb67fc054e28ed55744ecaea794543bdbb
codesparrow6 <57647352+codesparrow6@users.noreply.github.com>
2019-11-21 18:01:19 +0530

    Uploaded Multi-Threading information

---
+++ Manju_Shared_Info_on_java/Thread_Prgs/Thread_Prgs/concurrentutil/ConcurrentMapImplementations.java
@@ -0,0 +1,30 @@
+package com.manju.concurrentutil;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ConcurrentMapImplementations {
+
+	/**
+	 * ConcurrentMap interface represents a Map which is capable of handling concurrent access (puts and gets) to it.
+	 * 
+	 * ConcurrentHashMap is the only implementation of ConcurrentMap
+	 * 
+	 * ConcurrentHashMap does not lock the Map while you are reading from it. 
+	 * Additionally, ConcurrentHashMap does not lock the entire Map when writing to it. 
+	 * It only locks the part of the Map that is being written to, internally.
+	 * 
+	 * Another difference is that ConcurrentHashMap does not throw ConcurrentModificationException 
+	 * if the ConcurrentHashMap is changed while being iterated. The Iterator is not designed to be used by more than one thread though.
+	 */
+	public static void main(String[] args) {
+
+		ConcurrentMap concurrentMap = new ConcurrentHashMap();
+
+		concurrentMap.put("key", "values");
+		Object value = concurrentMap.get("key");
+
+		System.out.println(value);
+	}
+
+}
